a350e7f0e1ec6f5412d5dc1c592fbe68f750be23
Виталий Никонов <109771872+LordKisik@users.noreply.github.com>
2022-12-25 15:37:32 +0300

    Update Spiral.py

--- Spiral.py
+++ Spiral.py
@@ -15,31 +15,31 @@
 # считаем и присваиваем элементам матрицы соответствующие им значения.
 # (44-45) Выводим матрицу на экран.
 
-n = int(input('Введите n: '))
-matrix_n_x_n = [[0] * n for i in range(n)]
-for i in range(len(matrix_n_x_n)):
-    for j in range(len(matrix_n_x_n)):
-
+n = int(input())
+a = [[0] * n for i in range(n)]
+for i in range(n):
+    for j in range(n):
+         
         if i <= j and i + j < n:
             if i == 0:
-                matrix_n_x_n[i][j] = i + j + 1
+                a[i][j] = i + j + 1
             elif i > 0:
-                matrix_n_x_n[i][j] = matrix_n_x_n[i - 1][j] - 8 * i + 4 * n + 3
-
-        if i <= j and i + j >= n:
+                a[i][j] = a[i - 1][j] + 8 * (n - i) - (4 * n - 3)
+                  
+        if i <= j and i + j > n - 1:
             if j == n - 1:
-                matrix_n_x_n[i][j] = i + j + 1
+                a[i][j] = i + j + 1
             elif j < n - 1:
-                matrix_n_x_n[i][j] = (matrix_n_x_n[i - 1][j + 1]
-                                      + 8 * j - 4 * n + 10)
-
+                a[i][j] = a[i - 1][j + 1] + (8 * j - (5 * (n - 2) - n))
+                  
         if i > j and i + j < n:
             if j == 0:
-                matrix_n_x_n[i][j] = 4 * n - i - 3
+                a[i][j] = 4 * n - i - 3
             elif j > 0:
-                matrix_n_x_n[i][j] = matrix_n_x_n[i][j - 1] - 8 * j + 4 * n - 3
-
+                a[i][j] = a[i][j - 1] + 8 * (n - j) - (4 * n + 3)
+                  
         if i > j and i + j >= n:
-            matrix_n_x_n[i][j] = matrix_n_x_n[i - 1][j] - 8 * i + 4 * n + 1
-        print(matrix_n_x_n[i][j], '', end='')
-    print()
+            a[i][j] = a[i - 1][j] - 8 * i + (4 * n + 1)
+            
+        print(a[i][j], end="\t")
+    print("\n")
